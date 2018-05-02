dd8d98346e0a23ffcf025f19bd3baed84a23f0dc
Sol Sulanchupakorn <soliet@gmail.com>
2018-05-01 17:15:53 -0700

    Additional lessons

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-testtestetesttesttesttesttest
+// console.log("hi");
+// console.info("test")
+
+let x = 7;
+let y = 3;
+let z = x + y;
+console.log('Answer: ' + z);
---
+++ datatypes.js
@@ -0,0 +1,12 @@
+/*let x = 7; //integer
+console.log(typeof x);
+let y = false; // true or false
+console.log(typeof y);
+let z = 'hello world'; // single quotes
+console.log(typeof z);
+*/
+let a;
+console.log(a);
+console.log(typeof a);
+
+console.log('9')
